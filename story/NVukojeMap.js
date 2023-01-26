82ae4276f31638e47936a8ad04008dd6481a4d34
nvukoje <nvukoje@flemingcollege.ca>
2023-01-26 18:40:46 -0500

    new coordinates for story

--- story/NVukojeMap.js
+++ story/NVukojeMap.js
@@ -1,14 +1,14 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 5,
-      center: { lat: 53.329, lng: -33.900 },
+      zoom: 4.85,
+      center: { lat: 56.825, lng: -30.992 },
       mapTypeId: "terrain",
     });
     const flightPlanCoordinates = [
-      { lat: 51.470, lng: -1.014 },
-      { lat: 52.310, lng: 4.766 },
-      { lat: 52.260, lng: 20.837 },
-      { lat: 47.461, lng: 8.549 },
+      { lat: 64.142, lng: -21.929 },
+      { lat: 62.016, lng: -6.864 },
+      { lat: 59.917, lng: 10.725 },
+      { lat: 61.150, lng: -45.518 },
     ];
     const flightPath = new google.maps.Polyline({
       path: flightPlanCoordinates,
