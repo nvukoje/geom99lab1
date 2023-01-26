81b904e204980bfd0689f6c1ecd9290628d95ddd
nvukoje <nvukoje@flemingcollege.ca>
2023-01-26 18:01:55 -0500

    map relocation and title change

--- story/NVukojeMap.js
+++ story/NVukojeMap.js
@@ -1,7 +1,7 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 3,
-      center: { lat: 0, lng: -180 },
+      center: { lat: 49.503, lng: 8.594 },
       mapTypeId: "terrain",
     });
     const flightPlanCoordinates = [
