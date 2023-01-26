2dce6d3732046a5bcb2f3a9b6a0e399e8e8b8c7d
nvukoje <nvukoje@flemingcollege.ca>
2023-01-26 18:55:00 -0500

    plotting out new spots

--- story/NVukojeMap.js
+++ story/NVukojeMap.js
@@ -8,7 +8,10 @@ function initMap() {
       { lat: 64.142, lng: -21.929 },
       { lat: 62.016, lng: -6.864 },
       { lat: 59.917, lng: 10.725 },
+      { lat: 62.016, lng: -6.864 },
+      { lat: 64.142, lng: -21.929 },
       { lat: 61.150, lng: -45.518 },
+      { lat: 52.561, lng: -55.546 },
     ];
     const flightPath = new google.maps.Polyline({
       path: flightPlanCoordinates,
